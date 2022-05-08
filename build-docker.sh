1235c7aa6e378dd19aab6d927ffd788e76fb3374
Masterain <i@irain.in>
2022-05-07 22:06:47 -0700

    Create build-docker.sh

---
+++ build-docker.sh
@@ -0,0 +1,5 @@
+docker build -f Dockerfile -t qcloud-logger/latest .
+docker run --name=Qcloud-Logger \
+           --mount type=bind,source="$(pwd)"/data,target=/app/data \
+           --mount type=bind,source="$(pwd)"/config.json,target=/app/config.json \
+           qcloud-logger/latest
